5422e6f12c9ab7de8f706660a8ff232c4d1cb45d
ge78qad <ge78qad@tum.de>
2022-07-15 13:19:42 +0200

    Largest number

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,17 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
             return "Master";
-        } else if (query.contains("add")) {
-            return "Master";
+        } else if (query.contains("largest")) {
+            String s[] = query.split("largest: ");
+            String numbers[] = s[1].split(", ");
+            int largest = Integer.valueOf(numbers[0]);
+            for (String string : numbers) {
+                if (Integer.valueOf(string) > largest) {
+                    largest = Integer.valueOf(string);
+                }
+            }
+            return String.valueOf(largest);
+
         } else if (query.contains("sub")) {
             return "Master";
         } else if (query.contains("mul")) {
